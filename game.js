42ac377d010f490789e0be36d856ac806798af60
Benjamin Warner <benjaminnwarner@gmail.com>
2015-11-13 16:10:32 -0500

    Restructured Game class
    Changed from an object literal to a function. unit-tests.js updated to
    work with the changes

--- game.js
+++ game.js
@@ -1,29 +1,21 @@
 //Game class
 
-var Game = {
-	rollNumber: 0,
-	rolls: [],
+function Game(){
+	this.rollNumber = 0;
+	this.rolls = [];
 
-	create: function(){
-		var obj = Object.create(this);
-		obj.rollNumber = 0;
-		obj.rolls = [];
-		return this;
-	},
-
-	roll: function(pins){
+	this.roll = function(pins){
 		this.rolls[this.rollNumber] = pins;
-		this.rollNumber += 1;
-	},
+		this.rollNumber++;
+	};
 
-	score: function(){
+	this.score = function(){
 
 		var total = 0,
 			frame = 0;
 		for(var i = 0; i < 10; i++){
 			if(this.rolls[frame] == 10){
 				total += this.rolls[frame] + this.rolls[frame+1] + this.rolls[frame+2];
-				console.log(total,frame);
 				frame++;
 			}
 			else if(this.rolls[frame] + this.rolls[frame+1] == 10){
@@ -38,5 +30,4 @@ var Game = {
 		}
 		return total;
 	}
-
-};
+}
--- unit-tests.js
+++ unit-tests.js
@@ -2,12 +2,12 @@
 
 window.onload = function(){
 
-	var test_Perfect = Game.create();
+	var test_Perfect = new Game();
 	for(var i = 0; i < 12; i++)
 		test_Perfect.roll(10)
 	console.log(test_Perfect.score());
 
-	var test_Meh = Game.create();
+	var test_Meh = new Game();
 	for(var i = 0; i < 21; i++)
 		test_Meh.roll(5);
 	console.log(test_Meh.score());
