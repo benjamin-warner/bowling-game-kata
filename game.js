cff2bcdbfa93e0cb4b1a3de5e5cd76a786e914cb
Benjamin Warner <benjaminnwarner@gmail.com>
2015-11-12 21:30:05 -0500

    Game class written
    Unit tests are being weird. But it seems the class itself works.

--- game.js
+++ game.js
@@ -1,4 +1,42 @@
-var game = {
-	
+//Game class
+
+var Game = {
+	rollNumber: 0,
+	rolls: [],
+
+	create: function(){
+		var obj = Object.create(this);
+		obj.rollNumber = 0;
+		obj.rolls = [];
+		return this;
+	},
+
+	roll: function(pins){
+		this.rolls[this.rollNumber] = pins;
+		this.rollNumber += 1;
+	},
+
+	score: function(){
+
+		var total = 0,
+			frame = 0;
+		for(var i = 0; i < 10; i++){
+			if(this.rolls[frame] == 10){
+				total += this.rolls[frame] + this.rolls[frame+1] + this.rolls[frame+2];
+				console.log(total,frame);
+				frame++;
+			}
+			else if(this.rolls[frame] + this.rolls[frame+1] == 10){
+				total += this.rolls[frame] + this.rolls[frame+1];
+				frame += 2;
+			}
+			else{
+				total += this.rolls[frame] + this.rolls[frame+1];
+				frame += 2;
+			}
+		
+		}
+		return total;
+	}
 
 };
--- unit-tests.js
+++ unit-tests.js
@@ -1,3 +1,15 @@
-window.onload() = function{
+//Unit tests
+
+window.onload = function(){
+
+	var test_Perfect = Game.create();
+	for(var i = 0; i < 12; i++)
+		test_Perfect.roll(10)
+	console.log(test_Perfect.score());
+
+	var test_Meh = Game.create();
+	for(var i = 0; i < 21; i++)
+		test_Meh.roll(5);
+	console.log(test_Meh.score());
 
 }
